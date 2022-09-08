1043f7d2e54353cc9f9cc0e6e29e1af7d0a56c51
yanasekenta <ynsknt0505@gmail.com>
2022-09-08 17:24:24 +0900

    ios&androidを作成

---
+++ android/app/src/main/java/ionic/test/starter/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.test.starter;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'ionic.test.starter',
   appName: 'ionic-lesson-zenn',
   webDir: 'dist',
   bundledWebRuntime: false
